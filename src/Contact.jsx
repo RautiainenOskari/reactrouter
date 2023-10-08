ebf878a310721940f979a119b5ae63359463f503
Oskari Rautiainen <rautiainen.oskarir@gmail.com>
2023-10-08 15:32:50 +0300

    3 React komponenttia

---
+++ src/Contact.jsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+    return(<h1>Email: test@gmail.com</h1>);
+}
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Contact from './Contact.jsx'
+import About from './About.jsx'
+import Home from './Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <Home />,
+        index: true
+      },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
